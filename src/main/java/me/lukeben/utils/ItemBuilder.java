ab9f7e292fd25672be6811978e14c4dfb7c418b8
LukeBen <Lukewassung@yahoo.com>
2021-12-31 20:26:11 -0500

    Created a way for custom base64 skulls, while still using spigot-api.

--- src/main/java/me/lukeben/utils/ItemBuilder.java
+++ src/main/java/me/lukeben/utils/ItemBuilder.java
@@ -1,11 +1,13 @@
 package me.lukeben.utils;
 
 import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTGameProfile;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import de.tr7zw.changeme.nbtapi.NBTListCompound;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Singular;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -15,6 +17,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -65,11 +68,30 @@ public class ItemBuilder {
         }
 
         public Builder skull(String base64) {
+            SkullMeta menu = (SkullMeta) ItemBuilder.builder().toItemStack().getItemMeta();
             NBTItem nbtItem = new NBTItem(current);
             NBTCompound skull = nbtItem.addCompound("SkullOwner");
-            skull.setString("Id", UUID.randomUUID().toString());
+            int[] version = Arrays.stream(Bukkit.getBukkitVersion().substring(0, Bukkit.getBukkitVersion().indexOf("-")).split("\\.")).mapToInt(Integer::parseInt).toArray();
+            UUID randomUUID = UUID.randomUUID();
+            if (version[1] > 16) {
+                StringBuilder result = new StringBuilder();
+                long msb = randomUUID.getMostSignificantBits();
+                long lsb = randomUUID.getLeastSignificantBits();
+                String uuidString = result.append("[I;")
+                        .append(msb >> 32)
+                        .append(',')
+                        .append(msb & Integer.MAX_VALUE)
+                        .append(',')
+                        .append(lsb >> 32)
+                        .append(',')
+                        .append(lsb & Integer.MAX_VALUE)
+                        .append(']')
+                        .toString();
+                skull.setString("Id", uuidString);
+            } else {
+                skull.setString("Id", randomUUID.toString());
+            }
             NBTListCompound texture = skull.addCompound("Properties").getCompoundList("textures").addCompound();
-            texture.setString("Signature", "XpRfRz6/vXE6ip7/vq+40H6W70GFB0yjG6k8hG4pmFdnJFR+VQhslE0gXX/i0OAGThcAVSIT+/W1685wUxNofAiy+EhcxGNxNSJkYfOgXEVHTCuugpr+EQCUBI6muHDKms3PqY8ECxdbFTUEuWxdeiJsGt9VjHZMmUukkGhk0IobjQS3hjQ44FiT1tXuUU86oAxqjlKFpXG/iXtpcoXa33IObSI1S3gCKzVPOkMGlHZqRqKKElB54I2Qo4g5CJ+noudIDTzxPFwEEM6XrbM0YBi+SOdRvTbmrlkWF+ndzVWEINoEf++2hkO0gfeCqFqSMHuklMSgeNr/YtFZC5ShJRRv7zbyNF33jZ5DYNVR+KAK9iLO6prZhCVUkZxb1/BjOze6aN7kyN01u3nurKX6n3yQsoQQ0anDW6gNLKzO/mCvoCEvgecjaOQarktl/xYtD4YvdTTlnAlv2bfcXUtc++3UPIUbzf/jpf2g2wf6BGomzFteyPDu4USjBdpeWMBz9PxVzlVpDAtBYClFH/PFEQHMDtL5Q+VxUPu52XlzlUreMHpLT9EL92xwCAwVBBhrarQQWuLjAQXkp3oBdw6hlX6Fj0AafMJuGkFrYzcD7nNr61l9ErZmTWnqTxkJWZfZxmYBsFgV35SKc8rkRSHBNjcdKJZVN4GA+ZQH5B55mi4=");
             texture.setString("Value", base64);
             skullIdentifier(base64);
             return this;
