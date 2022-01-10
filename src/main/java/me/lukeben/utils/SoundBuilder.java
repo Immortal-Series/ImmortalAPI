35295102805803923dd5a3dc1686a6768cc6df0a
LukeBen <Lukewassung@yahoo.com>
2022-01-09 22:57:38 -0500

    Added SoundBuilder.java

---
+++ src/main/java/me/lukeben/utils/SoundBuilder.java
@@ -0,0 +1,40 @@
+package me.lukeben.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+@Getter
+@Builder(builderClassName = "Builder")
+public class SoundBuilder {
+
+    private final Sound SOUND;
+    private final float VOLUME;
+    private final float PITCH;
+
+    public static class Builder {
+
+        public Builder sound(Sound sound) {
+            SOUND(sound);
+            return this;
+        }
+
+        public Builder volume(float volume) {
+            VOLUME(volume);
+            return this;
+        }
+
+        public Builder pitch(float pitch) {
+            PITCH(pitch);
+            return this;
+        }
+
+    }
+
+    public void playSound(Player player) {
+        player.playSound(player.getLocation(), SOUND, VOLUME, PITCH);
+    }
+
+
+}
