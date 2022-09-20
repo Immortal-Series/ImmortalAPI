027d80b82978143e974a4560371494e978b71646
LukeBen <Lukewassung@yahoo.com>
2022-09-20 17:02:32 -0400

    Removed Settings from API

--- src/main/java/me/lukeben/ImmortalAPI.java
+++ src/main/java/me/lukeben/ImmortalAPI.java
@@ -4,11 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import me.lukeben.hooks.HookManager;
 import me.lukeben.menubuilder.MenuListener;
-import me.lukeben.settings.SettingsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
 
 public class ImmortalAPI {
 
@@ -24,9 +21,6 @@ public class ImmortalAPI {
         HookManager.getInstance().getHooks().forEach(hook -> {
             if(hook.isAvailable()) hook.onEnable();
         });
-        SettingsManager.getInstance().addEditableArray("LORE*", "This is an example lore line");
-        SettingsManager.getInstance().addEditableArray("FLAGS*", "HIDE_ENCHANTS");
-        SettingsManager.getInstance().addEditableArray("ENCHANTMENTS**", new HashMap<String, Integer>() {{put("DAMAGE_ALL", 1);}});
     }
 
 }
